2bf41b07313140cbe7fa53f1a12c014e0965b21f
karl wu <karlwuca@gmail.com>
2022-10-13 04:08:21 -0400

    final version

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,get_object_or_404,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, \
+    HttpResponseRedirect
 
 from .models import GeeksModel
 from .forms import GeeksForm
-
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
 
@@ -27,29 +28,40 @@ PS C:/Users/23584/Desktop/djangoProject> python manage.py shell
 >>> GeeksModel.objects.create(title = "title3",description="description3").save() 
 >>>   
 '''
+
+
 def list_view(request):
     context = {}
     context["dataset"] = GeeksModel.objects.all()
     return render(request, "list_view.html", context)
 
-def detail_view(request,id):
+
+def detail_view(request, id):
     context = {}
 
-    context['data']=GeeksModel.objects.get(id=id)
-    return render(request,'detail_view.html',context)
+    context['data'] = GeeksModel.objects.get(id=id)
+    return render(request, 'detail_view.html', context)
 
-def update_view(request,id):
-    context={}
 
-    obj = get_object_or_404(GeeksModel,id=id)
-    form = GeeksForm(request.POST or None,instance=obj)
+def update_view(request, id):
+    context = {}
+
+    obj = get_object_or_404(GeeksModel, id=id)
+    form = GeeksForm(request.POST or None, instance=obj)
 
     if form.is_valid():
         form.save()
-        return HttpResponseRedirect("/"+id)
+        return HttpResponseRedirect("/" + id)
 
-    context["form"]=form
-    return render(request,"update_view.html",context)
+    context["form"] = form
+    return render(request, "update_view.html", context)
+
+class GeeksDeleteView(DeleteView):
+    model = GeeksModel
+    success_url = "/"
+
+    template_name = "geeks/geeksmodel_confirm_delete.html"
+
 
 # def index(request):
 #     return None
