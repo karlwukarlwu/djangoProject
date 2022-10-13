ed7588631638b2c4344efbcc6563135f3873f0df
karl wu <karlwuca@gmail.com>
2022-10-13 03:02:25 -0400

    retrieve function

--- projectApp/views.py
+++ projectApp/views.py
@@ -6,7 +6,7 @@ from .forms import GeeksForm
 
 # Create your views here.
 
-def creat_view(request):
+def create_view(request):
     context = {}
 
     form = GeeksForm(request.POST or None)
@@ -15,6 +15,22 @@ def creat_view(request):
 
     context['form'] = form
     return render(request, 'create_view.html', context)
+
+'''
+PS C:/Users/23584/Desktop/djangoProject> python manage.py shell
+
+>>> from projectApp.models import GeeksModel
+
+>>> GeeksModel.objects.create(title = "title1",description="description1").save() 
+>>> GeeksModel.objects.create(title = "title2",description="description2").save()  
+>>> GeeksModel.objects.create(title = "title3",description="description3").save() 
+>>>   
+'''
+def list_view(request):
+    context = {}
+    context["dataset"] = GeeksModel.objects.all()
+    return render(request, "list_view.html", context)
+
 
 # def index(request):
 #     return None
